204744ab80c7739e7741ce406585f3f950e20bc0
feodoritiy <nikonovfedor36936@gmail.com>
2019-11-28 16:53:02 +0300

    added plugins in wp-conf, and index files in src, src/uikit

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
-let path = require('path');
+let path = require('path'),
+    HTMLWebpackPlugin = require('html-webpack-plugin'),
+    { CleanWebpackPlugin } = require('clean-webpack-plugin'),
+    SASSWebpackPlugin = require('sass-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -20,6 +23,21 @@ module.exports = {
             chunks: 'all',
         },
     },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HTMLWebpackPlugin({
+            filename: 'uikit/index.html',
+            template: 'src/uikit/index.pug',
+        }),
+        new HTMLWebpackPlugi({
+            filename: 'index.html',
+            template: 'src/index.pug',
+        }),
+        new SASSWebpackPlugin({
+            'uikit/index.scss': 'uikit/index.css',
+            'index.scss': 'index.css',
+        }),
+    ],
     module: {
         rules: [
             {
