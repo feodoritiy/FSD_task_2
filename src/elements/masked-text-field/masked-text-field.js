37483bb0bb5063581655f5af3fa99649dbf5cb9f
feodoritiy <nikonovfedor36936@gmail.com>
2019-11-30 16:05:47 +0300

    basicInput got color property; masked-text-field use russian date placholder

--- src/elements/masked-text-field/masked-text-field.js
+++ src/elements/masked-text-field/masked-text-field.js
@@ -2,7 +2,7 @@ import Inputmask from "inputmask";
 export function maskAllMaskedTextFileds() {
     let mask = new Inputmask('datetime', {
         inputFormat: 'dd.mm.yyyy',
-        placeholder: 'DD.MM.YYYY',
+        placeholder: 'ДД.ММ.ГГГГ',
     });
     document.querySelectorAll("input.masked-text-field").forEach(el => {
         mask.mask(el);
