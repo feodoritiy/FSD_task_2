50f50e36c1c429fd25579b343bb3fb13ee44a40b
feodoritiy <nikonovfedor36936@gmail.com>
2019-11-30 12:06:48 +0300

    installed inputmask by npm and using it in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import dropdown_image from "./elements/dropdown/keyboard_arrow_down-24px.svg";
+import "inputmask";
+import { maskAllMaskedTextFileds } from "./elements/masked-text-field/masked-text-field.js";
+maskAllMaskedTextFileds();
 console.log(dropdown_image);
