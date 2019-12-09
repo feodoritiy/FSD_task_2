e6d695f3875c85f85db3c4784d31963750681a1f
feodoritiy <nikonovfedor36936@gmail.com>
2019-12-09 13:23:06 +0300

    service-config-dropdown got shadow and new function interface (expand/collapse/toggle functions)

--- src/elements/service-config-dropdown/service-config-dropdown.js
+++ src/elements/service-config-dropdown/service-config-dropdown.js
@@ -18,17 +18,27 @@ $(document).ready(() => {
     let namespace = '.service-config-dropdown';
     document.querySelectorAll(namespace).forEach(dd => { //DropDown
         let ddServices = dd.querySelector(namespace + '__dropdown-services');
+        let header = ddServices.previousElementSibling;
         $(ddServices).hide(0);
 
-        ddServices.isOpened = false;
-        dd.querySelector(namespace + '__text-field button').addEventListener('click', e => {
-            if (ddServices.isOpened) {
-                e.target.parentElement.style.borderColor = "";
+        dd.isOpened = false;
+        dd.expand = e => {
+            header.style.borderColor = "rgba(31, 32, 65, 0.5)";
+            dd.isOpened = true;
+            $(ddServices).slideDown(1000);
+        };
+        dd.collapse = e => {
+            header.style.borderColor = "";
+            dd.isOpened = false;
+            $(ddServices).slideUp(1000);
+        };
+        dd.toggle = e => {
+            if (dd.isOpened) {
+                dd.collapse();
             } else {
-                e.target.parentElement.style.borderColor = "rgba(31, 32, 65, 0.5)";
+                dd.expand();
             }
-            ddServices.isOpened = !ddServices.isOpened;
-            $(ddServices).slideToggle(1000);
-        });
-    })
+        };
+        dd.querySelector(namespace + '__text-field button').addEventListener('click', dd.toggle);
+    });
 });
